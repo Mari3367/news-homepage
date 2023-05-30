fbd031cb6f2300ac1aaa9624ddad494a235ad23c
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2023-05-30 19:52:47 +0400

    applied styles

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// const wrappers = document.querySelectorAll('.new-unit-wrap');
+
+// wrappers.forEach((wrapper) => {
+// const border = document.createElement('div');
+// border.style.height = '1px';
+// border.style.backgroundColor = 'red';
+// border.className = 'border';
+//    wrapper.after(border);
+// });
+
+
+
+
